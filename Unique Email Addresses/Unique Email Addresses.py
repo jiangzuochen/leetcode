6e4a9c92152b7d7d758455e3f1aa28b640bcb187
zuochenjiang <zuochenjiang@sina.com>
2018-11-03 12:20:09 +0800

    Add readme.md for Unique Email Addresses

--- Unique Email Addresses/Unique Email Addresses.py
+++ Unique Email Addresses/Unique Email Addresses.py
@@ -1,1 +1,9 @@
-Unique Email Addresses
+class Solution:
+    def numUniqueEmails(self, emails):
+        """
+        :type emails: List[str]
+        :rtype: int
+        """
+        # emailsSet = set()
+        return len(set([x.split('@')[0].split('+')[0].replace('.','') \
+                        +'@'+ x.split('@')[1] for x in emails]))
